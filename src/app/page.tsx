ba492ca82045007846eb0f50c9e75dd00f402fd4
dndeveloper-saurabh <saurabh@pustack.com>
2024-08-08 14:37:29 +0530

    event sidebar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,190 +1,241 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
-import { FaCalendarCheck, FaUser } from "react-icons/fa6";
-import { IoMdMap } from "react-icons/io";
+const NoEventIcon = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    width={20}
+    height={20}
+    style={{
+      enableBackground: "new 0 0 512 512",
+    }}
+    viewBox="0 0 30 30"
+    {...props}
+  >
+    <path
+      d="M17.796 15.345c-.42-.42-1.07-.41-1.48 0l-1.31 1.31-1.31-1.31c-.41-.41-1.09-.41-1.5 0a1.088 1.088 0 0 0 0 1.5l1.31 1.31-1.3 1.3c-.21.21-.31.47-.31.74 0 .26.1.54.31.75.41.41 1.07.41 1.48 0l1.31-1.31 1.31 1.31c.41.41 1.07.41 1.48 0 .42-.41.42-1.08.01-1.49l-1.31-1.31 1.31-1.31c.41-.41.41-1.08 0-1.49z"
+      data-original="#000000"
+    />
+    <path
+      d="M24.59 4.27h-.4v2.52c0 1.65-1.34 3-3 3-1.65 0-3-1.35-3-3V4.27h-6.38v2.52c0 1.65-1.35 3-3 3-1.66 0-3-1.35-3-3V4.27h-.4C3.53 4.27 2 5.8 2 7.68v16.16c0 1.88 1.53 3.41 3.41 3.41h19.18c1.88 0 3.41-1.53 3.41-3.41V7.68c0-1.88-1.53-3.41-3.41-3.41zm0 20.98H5.41c-.78 0-1.41-.63-1.41-1.41V11.8h22v12.04c0 .78-.63 1.41-1.41 1.41z"
+      data-original="#000000"
+    />
+    <path
+      d="M8.807 7.787a1 1 0 0 0 1-1V3.75a1 1 0 1 0-2 0v3.036a1 1 0 0 0 1 1zm12.385 0a1 1 0 0 0 1-1V3.75a1 1 0 1 0-2 0v3.036a1 1 0 0 0 1 1z"
+      data-original="#000000"
+    />
+  </svg>
+);
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-primary flex items-center justify-center">
-      <div className="w-full max-w-[900px] mx-auto py-2 h-[80vh] overflow-auto">
-        <hr className="border-dashed border-[#1f1d1a4d] mt-[20px]" />
-        <hr className="border-dashed border-[#1f1d1a4d] mt-[1px]" />
-
-        <div className="mt-8">
-          <div className="styles_title">
-            <p>
-              <span className="inline-flex mr-2">
-                <FaCalendarCheck />
-              </span>
-              Description
-            </p>
+    <div className="w-screen h-screen bg-primary flex items-center justify-center font-featureRegular">
+      <div className="w-full max-w-[400px] bg-lightPrimary h-[100vh] overflow-auto">
+        <section>
+          <div className="w-full h-[100px] bg-july bg-cover overlay-black text-white flex items-center justify-center">
+            <span className="block relative text-3xl font-[600] font-helvetica">
+              July
+            </span>
           </div>
-        </div>
-        <div>
-          <p>
-            Donald John Trump (born June 14, 1946) is an American politician,
-            media personality, and businessman who served as the 45th
-          </p>
-        </div>
-
-        <div className="mt-8">
-          <div className="styles_divider"></div>
-          <div className="styles_title">
-            <p>
-              <span className="inline-flex mr-2">
-                <FaUser />
-              </span>
-              Organizer
-            </p>
-          </div>
-        </div>
-        <div className="mt-5">
-          <div className="border divide-y divide-dashed divide-[#1f1d1a65] border-dashed border-[#1f1d1a] rounded-2xl bg-lightPrimary">
-            <div className="flex items-center gap-2 p-4">
-              <div>
-                <Image
-                  className="w-6 md:w-8 h-6 md:h-8 rounded-full"
-                  width={30}
-                  height={30}
-                  src="https://content.jdmagicbox.com/comp/ernakulam/m4/0484px484.x484.140206113128.a9m4/catalogue/we-create-events-panampilly-nagar-ernakulam-event-management-companies-nsobpzm660.jpg"
-                  alt="Nothing"
-                />
+          <div className="py-2">
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-1 items-center font-helvetica justify-start">
+                <p className="text-xs">TUE</p>
+                <p className="text-xl font-medium">9</p>
               </div>
-              <div>
-                <span className="text-base md:text-lg font-featureHeadline">
-                  Tech Innovators Inc.
-                </span>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-green-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
               </div>
             </div>
-            <div className="flex-1 p-4">
-              <p className="text-sm mb-1 text-appBlack text-opacity-60">
-                Donald John Trump (born June 14, 1946) is an American
-                politician, media personality, and businessman who served as the
-                45th Donald John Trump (born June 14, 1946) is sman who served
-                as the 45th Donald John Trump (born June 14, 1946) is an
-                American politician, media personality, and businessman who
-                served as the 45th
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="mt-8">
-          <div className="styles_divider"></div>
-          <div className="styles_title">
-            <p>
-              <span className="inline-flex mr-2">
-                <FaUser />
-              </span>
-              Date & Time
-            </p>
-          </div>
-        </div>
-        <div className="mt-5">
-          <div className="flex items-center p-4 border divide-x divide-dashed divide-[#1f1d1a19] border-dashed border-[#1f1d1a] rounded-2xl bg-lightPrimary">
-            <div className="flex-1">
-              <p className="text-sm mb-1 text-appBlack text-opacity-60">
-                Starts On:
-              </p>
-              <p>
-                <b>September 07, 2024</b>
-              </p>
-              <p>
-                <b>09:00 AM</b>
-              </p>
-            </div>
-            <div className="flex-1 pl-4">
-              <p className="text-sm mb-1 text-appBlack text-opacity-60">
-                Ends On:
-              </p>
-              <p>
-                <b>September 07, 2024</b>
-              </p>
-              <p>
-                <b>11:00 AM</b>
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div className="mt-8">
-          <div className="styles_divider"></div>
-          <div className="styles_title">
-            <p>
-              <span className="inline-flex mr-2">
-                <FaUser />
-              </span>
-              Venue
-            </p>
-          </div>
-        </div>
-        <div className="mt-5">
-          {/* <div className="flex cursor-pointer items-center before:absolute relative before:top-0 before:left-0 before:w-full before:h-full before:bg-black before:bg-opacity-60 overflow-hidden before:z-[2] border divide-x divide-dashed divide-[#1f1d1a19] border-dashed border-[#1f1d1a] rounded-2xl bg-lightPrimary">
-            <Image
-              className="w-full aspect-video rounded-2xl"
-              width={1000}
-              height={1000}
-              src="https://content.jdmagicbox.com/comp/ernakulam/m4/0484px484.x484.140206113128.a9m4/catalogue/we-create-events-panampilly-nagar-ernakulam-event-management-companies-nsobpzm660.jpg"
-              alt="Nothing"
-            />
-            <div className="absolute bottom-3 md:bottom-5 w-full z-10 px-3 md:px-8 flex items-center justify-between">
-              <div className="text-white">
-                <h3 className="font-featureHeadline text-[18px] md:text-[25px]">
-                  Courtyard, Downtown
-                </h3>
-                <p className="text-white text-[12px] md:text-[16px] text-opacity-40">
-                  Open in maps
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-0 items-center font-helvetica justify-start">
+                <p className="text-xs text-danger-800">TUE</p>
+                <p className="text-lg font-medium w-8 h-8 rounded-full bg-danger-800 text-white flex items-center justify-center">
+                  9
                 </p>
               </div>
-              <div className="w-10 h-10 text-xl text-gray-400 border border-gray-500 rounded-full bg-gray-700 flex items-center justify-center">
-                <IoMdMap />
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-danger-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
               </div>
             </div>
-          </div> */}
-          <div className="p-4 border divide-x divide-dashed divide-[#1f1d1a19] border-dashed border-[#1f1d1a] rounded-2xl bg-lightPrimary">
-            <div className="flex-1">
-              <p className="text-sm text-appBlack text-opacity-60">
-                Meet link:
-              </p>
-              <Link href={"#"} target="_blank" className="text-appBlue">
-                <b>
-                  <u>https://meet.google.com</u>
-                </b>
-              </Link>
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-1 items-center font-helvetica justify-start">
+                <p className="text-xs">TUE</p>
+                <p className="text-xl font-medium">9</p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-orange-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="mt-8">
-          <div className="styles_divider"></div>
-          <div className="styles_title">
-            <p>
-              <span className="inline-flex mr-2">
-                <FaUser />
-              </span>
-              Contact
-            </p>
+        </section>
+        <section>
+          <div className="w-full h-[100px] bg-august bg-cover overlay-black text-white flex items-center justify-center">
+            <span className="block relative text-3xl font-[600] font-helvetica">
+              August
+            </span>
           </div>
-        </div>
-        <div className="mt-5">
-          <div className="p-4 border divide-x divide-dashed divide-[#1f1d1a19] border-dashed border-[#1f1d1a] rounded-2xl bg-lightPrimary">
-            <div className="flex-1">
-              <p className="text-sm text-appBlack text-opacity-60">Email:</p>
-              <p>
-                <b>saurs2000@gmail.com</b>
-              </p>
-            </div>
-            <div className="flex-1 mt-3">
-              <p className="text-sm text-appBlack text-opacity-60">Phone:</p>
-              <p>
-                <b>+91 93198-25600</b>
+          <div className="py-2">
+            <div>
+              <p className="text-sm flex items-center justify-center gap-3 py-3 text-appBlack font-helvetica text-opacity-65">
+                <NoEventIcon fill="#1f1d1a" />
+                <span className="-mb-0.5">No Events</span>
               </p>
             </div>
           </div>
-        </div>
+        </section>
+        <section>
+          <div className="w-full h-[100px] bg-september bg-cover overlay-black text-white flex items-center justify-center">
+            <span className="block relative text-3xl font-[600] font-helvetica">
+              September
+            </span>
+          </div>
+          <div className="py-2">
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-1 items-center font-helvetica justify-start">
+                <p className="text-xs">TUE</p>
+                <p className="text-xl font-medium">9</p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-green-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-0 items-center font-helvetica justify-start">
+                <p className="text-xs text-danger-800">TUE</p>
+                <p className="text-lg font-medium w-8 h-8 rounded-full bg-danger-800 text-white flex items-center justify-center">
+                  12
+                </p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-danger-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-1 items-center font-helvetica justify-start">
+                <p className="text-xs">TUE</p>
+                <p className="text-xl font-medium">9</p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-orange-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="w-full h-[100px] bg-october bg-cover overlay-black text-white flex items-center justify-center">
+            <span className="block relative text-3xl font-[600] font-helvetica">
+              October
+            </span>
+          </div>
+          <div className="py-2">
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-1 items-center font-helvetica justify-start">
+                <p className="text-xs">TUE</p>
+                <p className="text-xl font-medium">13</p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-green-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-0 items-center font-helvetica justify-start">
+                <p className="text-xs text-danger-800">TUE</p>
+                <p className="text-lg font-medium w-8 h-8 rounded-full bg-danger-800 text-white flex items-center justify-center">
+                  22
+                </p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-danger-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-1 items-center font-helvetica justify-start">
+                <p className="text-xs">TUE</p>
+                <p className="text-xl font-medium">9</p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-orange-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="w-full h-[100px] bg-november bg-cover overlay-black text-white flex items-center justify-center">
+            <span className="block relative text-3xl font-[600] font-helvetica">
+              November
+            </span>
+          </div>
+          <div className="py-2">
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-1 items-center font-helvetica justify-start">
+                <p className="text-xs">TUE</p>
+                <p className="text-xl font-medium">9</p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-green-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-0 items-center font-helvetica justify-start">
+                <p className="text-xs text-danger-800">TUE</p>
+                <p className="text-lg font-medium w-8 h-8 rounded-full bg-danger-800 text-white flex items-center justify-center">
+                  9
+                </p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-danger-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+            <div className="grid grid-cols-[50px_1fr] md:grid-cols-[70px_1fr] items-stretch w-full py-2 pl-0 md:pl-2 md:px-2 pr-2">
+              <div className="flex flex-col py-1 items-center font-helvetica justify-start">
+                <p className="text-xs">TUE</p>
+                <p className="text-xl font-medium">9</p>
+              </div>
+              <div className="flex-1">
+                <div className="w-full py-2 px-4 bg-orange-800 text-white rounded-xl text-sm font-helvetica">
+                  <p className="font-bold">Annual Tech Conference 2024</p>
+                  <p className="text-xs mt-1">9:00 AM - 11:00 AM</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
+      <div className="flex-1"></div>
     </div>
   );
 }
